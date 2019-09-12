d0a3d16e335a4a131a1e6b314b25eb1150dd4218
Faisal <faisal@rotatestudios.com>
2019-09-12 02:10:37 +0200

    Updates

---
+++ precache-manifest.24eddd2a2824415735232025d59e0409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe8502fb9963fcd56e2315710859cb9",
+    "url": "/gnome-town/index.html"
+  },
+  {
+    "revision": "ff5c4221b3ab3bbab5ad",
+    "url": "/gnome-town/static/css/main.94be9298.chunk.css"
+  },
+  {
+    "revision": "d98d919df03f7391cd29",
+    "url": "/gnome-town/static/js/2.a4431e6b.chunk.js"
+  },
+  {
+    "revision": "ff5c4221b3ab3bbab5ad",
+    "url": "/gnome-town/static/js/main.91512765.chunk.js"
+  },
+  {
+    "revision": "e396e00eef97e40061bb",
+    "url": "/gnome-town/static/js/runtime~main.90c07962.js"
+  }
+]);
